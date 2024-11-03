4251938e64773ce298bb6e51018205caaefaebc7
tauheed0110 <tauheed0110@gmail.com>
2024-11-03 12:45:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const makeChange = (c) => {
-  // your name here
-};
+const makeChange = (amount) => {
+    // your name here
+    amount = Number(amount)
+    const change = {q:0, d:0, n: 0,p :0}
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+    // check for the quorters
+    change.q = Math.floor(amount/25);
+    amount %= 25;
+    // check for dimes
+    change.d = Math.floor(amount/10);
+    amount %= 10;
+    // check for the nickles
+    change.n = Math.floor(amount/5);
+    amount %= 5;
+    // now rest is the piny
+    change.q = amount;
+
+    return change;
+
+  };
+  
+  // Do not the change the code below
+  const c = prompt("Enter c: ");
+  alert(JSON.stringify(makeChange(c)));
+  
